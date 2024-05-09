1498c9d78da7dc93110d9d4e6f326ddfcf6f9c34
Daniel Imbert <danielimbert90@gmail.com>
2024-05-09 12:51:18 +0300

    Push Project
    Diagnose and fix code that compiles, but fails to perform as expected
    Use common logic structures to control the execution of code.
    Implement appropriate data types
    Interpret simple code
    Improve simple code using the features of an IDE

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float shotDelay = 0.5f;
+    private float timeUntilNextShot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilNextShot -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeUntilNextShot <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeUntilNextShot = shotDelay;
+        }
+    }
+}
